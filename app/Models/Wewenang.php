300cfcb31d6557f4a3bec167be42bec65e65637f
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-05-21 20:46:18 +0700

    refactor: Create wewenang migration and model

---
+++ app/Models/Wewenang.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wewenang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wewenang';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function saranaPrasarana(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(SaranaPrasarana::class, 'id_wewenang');
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(User::class, 'id_wewenang');
+    }
+}
