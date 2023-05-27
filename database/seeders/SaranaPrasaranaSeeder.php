12337182eb5f9f9345e81a89c76590e776147737
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-05-27 19:10:17 +0700

    Update SarPras Seeder 3

--- database/seeders/SaranaPrasaranaSeeder.php
+++ database/seeders/SaranaPrasaranaSeeder.php
@@ -64,8 +64,16 @@ class SaranaPrasaranaSeeder extends Seeder
         );
         SaranaPrasarana::create(
             [
+                'id_wewenang' => '3',
+                'nama' => 'Laboratorium Komputer',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 15 Meja, 1 Proyektor, 1 AC, 30 Set Komputer',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
                 'id_wewenang' => '2',
-                'nama' => 'Aula Gedung Akuntansi',
+                'nama' => 'Auditorium Gedung AD',
                 'kapasitas' => '30',
                 'fasilitas' => '50 Kursi, 1 Proyektor, Speaker, 2 AC, Set Sound System',
             ]
@@ -92,13 +100,145 @@ class SaranaPrasaranaSeeder extends Seeder
                 'id_wewenang' => '3',
                 'nama' => 'Laboratorium Komputer',
                 'kapasitas' => '30',
-                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC, 30 Set Komputer',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 15 Meja, 1 Proyektor, 1 AC, 30 Set Komputer',
             ]
         );
         SaranaPrasarana::create(
             [
                 'id_wewenang' => '3',
                 'nama' => 'Multimedia Teknik Sipil',
+                'kapasitas' => '60',
+                'fasilitas' => '60 Kursi, 2 Proyektor, 4 AC, 4 Speaker, Set Sound System',
+            ]
+        );
+
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '4',
+                'nama' => 'AG 2.16B',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '4',
+                'nama' => 'AJ 3.17',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '4',
+                'nama' => 'Laboratorium Digital/Manajemen Rekayas',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 15 Meja, 1 Proyektor, 1 AC, 30 Set Komputer',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '4',
+                'nama' => 'Auditorium Gedung AH',
+                'kapasitas' => '100',
+                'fasilitas' => '100 Kursi, 1 Proyektor, 4 AC, 2 Speaker, Set Sound System',
+            ]
+        );
+
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '5',
+                'nama' => 'AB 01',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '5',
+                'nama' => 'AB 02',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '5',
+                'nama' => 'Laboratorium Komputer',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 15 Meja, 1 Proyektor, 1 AC, 30 Set Komputer',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '5',
+                'nama' => 'Auditorium Gedung AB',
+                'kapasitas' => '60',
+                'fasilitas' => '60 Kursi, 1 Proyektor, 2 AC, 2 Speaker, Set Sound System',
+            ]
+        );
+
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '6',
+                'nama' => 'Ruang Teori 5',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '6',
+                'nama' => 'Laboratorium RPL 4',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '6',
+                'nama' => 'Laboratorium Jaringan Komputer',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC, 30 Set Komputer',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '6',
+                'nama' => 'Auditorium Teknologi Informasi',
+                'kapasitas' => '90',
+                'fasilitas' => '90 Kursi, 24 Meja, 40 Set Komputer, 3 Proyektor, 4 AC, 4 Speaker, Set Sound System',
+            ]
+        );
+
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '7',
+                'nama' => 'AQ 4.02',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '7',
+                'nama' => 'AO 1.02',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 1 Proyektor, 1 AC',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '7',
+                'nama' => 'Laboratorium Operasi Teknik Kimia',
+                'kapasitas' => '30',
+                'fasilitas' => '1 Papan Tulis, 30 Kursi, 30 Meja, 1 AC, 30 Set Komputer, 6 Almari, 30 Set Alat Praktikum OTK',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '7',
+                'nama' => 'Auditorium Gedung AQ',
                 'kapasitas' => '60',
                 'fasilitas' => '60 Kursi, 2 Proyektor, 4 AC, 4 Speaker, Set Sound System',
             ]
@@ -142,6 +282,14 @@ class SaranaPrasaranaSeeder extends Seeder
                 'nama' => 'Graha Theater',
                 'kapasitas' => '80',
                 'fasilitas' => 'Tribun',
+            ]
+        );
+        SaranaPrasarana::create(
+            [
+                'id_wewenang' => '8',
+                'nama' => 'Auditorium Gedung Teknik Sipil',
+                'kapasitas' => '200',
+                'fasilitas' => '200 Kursi, 3 Proyektor, 8 AC, 8 Speaker, Set Sound System, 2 Toilet',
             ]
         );
     }
