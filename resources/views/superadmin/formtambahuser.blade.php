433a1073146f04bc572e9ed456ca1c2a724bfec4
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-01 22:51:46 +0700

    Change name input and add error condition

--- resources/views/superadmin/formtambahuser.blade.php
+++ resources/views/superadmin/formtambahuser.blade.php
@@ -8,59 +8,95 @@
         <div class="section-body ">
             <div id="layoutSidenav_content">
                 <main>
-            <div class="container-xl px-4 mt-5">
-                <div class="card mb-4">
-                    <div class="card-header bg-whitesmoke"><h4>Form Pendaftaran User Baru</h4></div>
-                    <div class="card-body ps-5 pe-15">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Nama User</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            </div>
+                    <div class="container-xl px-4 mt-5">
+                        <div class="card mb-4">
+                            <div class="card-header bg-whitesmoke"><h4>Form Pendaftaran User Baru</h4></div>
+                            <div class="card-body ps-5 pe-15">
+                                <form method="POST" action="{{ route('users.store') }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="nama" class="form-label">Nama User</label>
+                                        <input type="text" class="form-control @error('nama') is-invalid @enderror"
+                                               id="nama" name="nama" value="{{ old('nama') }}">
+                                        @error('nama')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
 
-                            <div class="row mb-3">
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Email User</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                </div>
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Password</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                </div>
-                            </div>
+                                    <div class="row mb-3">
+                                        <div class="col-lg-6 col-sm-6">
+                                            <label for="email" class="form-label">Email User</label>
+                                            <input type="email"
+                                                   class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                                            @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-lg-6 col-sm-6">
+                                            <label for="password" class="form-label">Password</label>
+                                            <input type="password"
+                                                   class="form-control @error('password') is-invalid @enderror"
+                                                   id="password"
+                                                   name="password">
+                                            @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                            <div class="row mb-3">
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Nomor Telepon</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                </div>
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Role</label>
-                                    <select class="form-control">
-                                        <option>Default select</option>
-                                        <option>Admin</option>
-                                        <option>User</option>
-                                    </select>
-                                </div>
-                            </div>
+                                    <div class="row mb-3">
+                                        <div class="col-lg-4 col-sm-4">
+                                            <label for="phone" class="form-label">Nomor Telepon</label>
+                                            <input type="text" class="form-control @error('phone') is-invalid @enderror"
+                                                   id="phone" name="phone" value="{{ old('phone') }}">
+                                            @error('phone')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-lg-4 col-sn-4">
+                                            <label for="jurusan" class="form-label">Jurusan</label>
+                                            <select class="form-control" name="wewenang_id">
+                                                @foreach($wewenangs as $wewenang)
+                                                    @if(old('wewenang_id') == $wewenang->id)
+                                                        <option value="{{$wewenang->id}}" selected>{{$wewenang->name}}</option>
+                                                    @endif
+                                                    <option value="{{$wewenang->id}}">{{$wewenang->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col-lg-4 col-sm-4">
+                                            <label for="Role" class="form-label">Role</label>
+                                            <select class="form-control" name="role">
+                                                <option>User</option>
+                                                <option>Super Admin</option>
+                                                <option>Admin</option>
+                                            </select>
+                                        </div>
+                                    </div>
 
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Bio</label>
-                                <textarea type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" rows="3"></textarea>
-                            </div>
 
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
+
+                                    <button type="submit" class="btn btn-primary mt-5">Submit</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
                 </main>
-        </div>
+            </div>
         </div>
     </section>
 @endsection
 
 @section('sidebar')
     @parent
-    <li><a href="{{route('kelola.superadmin')}}" class="nav-link"><i class="fas fa-file-alt"></i><span>Kelola Pengguna</span></a></li>
+    <li><a href="{{route('kelola.superadmin')}}" class="nav-link"><i
+                class="fas fa-file-alt"></i><span>Kelola Pengguna</span></a></li>
 @endsection
