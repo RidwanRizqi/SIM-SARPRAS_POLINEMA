e403848d172cdec1ea00ed3c1674bb6b01602307
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-02 21:59:32 +0700

    add fullcalendar feature

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('title', 'Form Daftar')
-
+@push('customCSS')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.css" />
+@endpush
 @section('content')
     <section class="section">
         <div class="section-header">
@@ -9,61 +11,182 @@
         </div>
 
         <div class="section-body ">
-            <div id="layoutSidenav_content">
-                <main>
-            <div class="container-xl px-4 mt-5">
-                <div class="card mb-4">
-                    <div class="card-header bg-whitesmoke"><h4>Form Pendaftaran Peminjaman Sarana &amp; Prasarana</h4></div>
-                    <div class="card-body ps-5 pe-15">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Nama Ruangan</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Nama Kegiatan</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div class="row">
+                <div class="col-xl-6 mb-4">
+                    <!-- Dashboard example card 1-->
+                    <a class="card lift h-100">
+                        <div class="card-header bg-whitesmoke"><h4>Daftar Peminjaman Pada Ruang {{ $sarpras->nama }}</h4></div>
+                        <div class="card-body d-flex justify-content-center flex-column">
+                            <div id='calendar'></div>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.js"></script>
+                            <script>
+                                $(document).ready(function() {
+                                    $('#calendar').fullCalendar({
+                                        defaultView: 'month',
+                                        events: [
+                                                @foreach ($peminjaman as $item)
+                                            {
+                                                title: '{{ $item->kegiatan }}',
+                                                start: '{{ $item->tanggal_mulai }}',
+                                                end: '{{ $item->tanggal_selesai }}',
+                                                color: 'green', // Ganti dengan warna blok yang diinginkan
+                                                textColor: 'white' // Ganti dengan warna font yang diinginkan
+                                            },
+                                            @endforeach
+                                        ]
+                                    });
+                                });
+                            </script>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-6 mb-4">
+                    <!-- Dashboard example card 1-->
+                    <a class="card lift h-100">
+                        <div class="card-header bg-whitesmoke"><h4>Form Pendaftaran Peminjaman Sarana &amp;
+                                Prasarana</h4></div>
+                        <div class="card-body d-flex justify-content-center flex-column">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="namaRuang" class="form-label">Nama Ruangan</label>
+                                    <input type="text" class="form-control" id="namaRuang" value="{{ $sarpras->nama }}" readonly>
                                 </div>
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="exampleInputEmail1" class="form-label">Penanggung Jawab</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <div class="row mb-3">
+                                    <div class="col-lg-6 col-sm-6">
+                                        <label for="exampleInputEmail1" class="form-label">Nama Kegiatan</label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1"
+                                               aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <label for="exampleInputEmail1" class="form-label">Penanggung Jawab</label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1"
+                                               aria-describedby="emailHelp">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="startDate" class="form-label">Mulai Tanggal</label>
-                                    <input id="startDate" class="form-control" type="date" />
-                                    <span id="startDateSelected"></span>
+                                <div class="row mb-3">
+                                    <div class="col-lg-6 col-sm-6">
+                                        <label for="startDate" class="form-label">Mulai Tanggal</label>
+                                        <input id="startDate" class="form-control" type="date"/>
+                                        <span id="startDateSelected"></span>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <label for="endDate" class="form-label">Sampai Tanggal</label>
+                                        <input id="endDate" class="form-control" type="date"/>
+                                        <span id="endDateSelected"></span>
+                                    </div>
                                 </div>
-                                <div class="col-lg-6 col-sm-6">
-                                    <label for="endDate" class="form-label">Sampai Tanggal</label>
-                                    <input id="endDate" class="form-control" type="date" />
-                                    <span id="endDateSelected"></span>
+                                <div class="mb-3">
+                                    <label for="uploadproposal" class="form-label">Upload Proposal</label>
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="uploadproposal" class="form-label">Upload Proposal</label>
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="customFile">
-                                    <label class="custom-file-label" for="customFile">Choose file</label>
-                                  </div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Fasilitas</label>
-                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input" readonly>
-                            </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Fasilitas</label>
+                                    <input type="text" id="disabledTextInput" class="form-control"
+                                           value="{{ $sarpras->fasilitas }}" readonly>
+                                </div>
 
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                            </script>
+                        </div>
+                    </a>
                 </div>
             </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-xl px-4 mt-5">
+                        <div class="card mb-4">
+{{--                            <div class="card-header bg-whitesmoke"><h4>Daftar Peminjaman Pada Ruang {{ $sarpras->nama }}</h4></div>--}}
+{{--                            <div class="card-body ps-5 pe-15">--}}
+{{--                                <div id='calendar'></div>--}}
+{{--                                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>--}}
+{{--                                <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>--}}
+{{--                                <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.js"></script>--}}
+{{--                                <script>--}}
+{{--                                    $(document).ready(function() {--}}
+{{--                                        $('#calendar').fullCalendar({--}}
+{{--                                            defaultView: 'month',--}}
+{{--                                            events: [--}}
+{{--                                                    @foreach ($peminjaman as $item)--}}
+{{--                                                {--}}
+{{--                                                    title: '{{ $item->kegiatan }} - {{ $item->penanggung_jawab }}',--}}
+{{--                                                    start: '{{ $item->tanggal_mulai }}',--}}
+{{--                                                    end: '{{ $item->tanggal_selesai }}',--}}
+{{--                                                    color: 'green', // Ganti dengan warna blok yang diinginkan--}}
+{{--                                                    textColor: 'white' // Ganti dengan warna font yang diinginkan--}}
+{{--                                                },--}}
+{{--                                                @endforeach--}}
+{{--                                            ]--}}
+{{--                                        });--}}
+{{--                                    });--}}
+{{--                                </script>--}}
+{{--                            </div>--}}
+{{--                            <div class="card-header bg-whitesmoke"><h4>Form Pendaftaran Peminjaman Sarana &amp;--}}
+{{--                                    Prasarana</h4></div>--}}
+{{--                            <div class="card-body ps-5 pe-15">--}}
+{{--                                <form>--}}
+{{--                                    <div class="mb-3">--}}
+{{--                                        <label for="exampleInputEmail1" class="form-label">Nama Ruangan</label>--}}
+{{--                                        <input type="text" class="form-control" id="exampleInputEmail1"--}}
+{{--                                               aria-describedby="emailHelp">--}}
+{{--                                    </div>--}}
+{{--                                    <div class="row mb-3">--}}
+{{--                                        <div class="col-lg-6 col-sm-6">--}}
+{{--                                            <label for="exampleInputEmail1" class="form-label">Nama Kegiatan</label>--}}
+{{--                                            <input type="text" class="form-control" id="exampleInputEmail1"--}}
+{{--                                                   aria-describedby="emailHelp">--}}
+{{--                                        </div>--}}
+{{--                                        <div class="col-lg-6 col-sm-6">--}}
+{{--                                            <label for="exampleInputEmail1" class="form-label">Penanggung Jawab</label>--}}
+{{--                                            <input type="text" class="form-control" id="exampleInputEmail1"--}}
+{{--                                                   aria-describedby="emailHelp">--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="row mb-3">--}}
+{{--                                        <div class="col-lg-6 col-sm-6">--}}
+{{--                                            <label for="startDate" class="form-label">Mulai Tanggal</label>--}}
+{{--                                            <input id="startDate" class="form-control" type="date"/>--}}
+{{--                                            <span id="startDateSelected"></span>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="col-lg-6 col-sm-6">--}}
+{{--                                            <label for="endDate" class="form-label">Sampai Tanggal</label>--}}
+{{--                                            <input id="endDate" class="form-control" type="date"/>--}}
+{{--                                            <span id="endDateSelected"></span>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mb-3">--}}
+{{--                                        <label for="uploadproposal" class="form-label">Upload Proposal</label>--}}
+{{--                                        <div class="custom-file">--}}
+{{--                                            <input type="file" class="custom-file-input" id="customFile">--}}
+{{--                                            <label class="custom-file-label" for="customFile">Choose file</label>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mb-3">--}}
+{{--                                        <label for="exampleInputPassword1" class="form-label">Fasilitas</label>--}}
+{{--                                        <input type="text" id="disabledTextInput" class="form-control"--}}
+{{--                                               placeholder="Disabled input" readonly>--}}
+{{--                                    </div>--}}
+
+{{--                                    <button type="submit" class="btn btn-primary">Submit</button>--}}
+{{--                                </form>--}}
+{{--                            </div>--}}
+                        </div>
+                    </div>
                 </main>
-        </div>
+            </div>
         </div>
     </section>
 @endsection
+@push('customJS')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.js"></script>
+@endpush
 
 @section('sidebar')
     @parent
