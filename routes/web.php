b65ededfdf0634822c3bccfd09f539cc9f2107dc
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-03 12:28:23 +0700

    Refactor Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PeminjamanUserController;
 use App\Http\Controllers\SarprasController;
 use App\Http\Controllers\UserSarprasController;
@@ -29,84 +30,84 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/home', function () {
         if (Auth::check()) {
             if (Auth::user()->role == 'superadmin') {
-                return view('dashboard.superadmin');
+                return (new DashboardController)->superadmin();
             } elseif (Auth::user()->role == 'admin') {
-                return view('dashboard.admin');
+                return (new DashboardController)->admin();
             } else {
-                return view('dashboard.home');
+                return (new DashboardController)->user();
             }
         } else {
             return view('auth.login');
         }
     })->name('home');
+});
 
 //    Route::get('edit-profile', function () {
 //        return view('dashboard.profile');
 //    })->name('profile.edit');
 
-    Route::get('edit-profile', function () {
-        if (Auth::check()) {
-            if (Auth::user()->role == 'superadmin') {
-                return view('superadmin.profile_Sadmin');
-            } elseif (Auth::user()->role == 'admin') {
-                return view('admin.profile_admin');
-            } else {
-                return view('user.profile_user');
-            }
+Route::get('edit-profile', function () {
+    if (Auth::check()) {
+        if (Auth::user()->role == 'superadmin') {
+            return view('superadmin.profile_Sadmin');
+        } elseif (Auth::user()->role == 'admin') {
+            return view('admin.profile_admin');
         } else {
-            return view('auth.login');
+            return view('user.profile_user');
         }
-    })->name('profile.edit');
+    } else {
+        return view('auth.login');
+    }
+})->name('profile.edit');
 
-    Route::middleware('can:superadmin')->group(function () {
-        Route::get('kelola-superadmin', function () {
-            return view('superadmin.kelola');
-        })->name('kelola.superadmin');
+Route::middleware('can:superadmin')->group(function () {
+    Route::get('kelola-superadmin', function () {
+        return view('superadmin.kelola');
+    })->name('kelola.superadmin');
 
-        Route::resource('users', UserController::class);
-    });
+    Route::resource('users', UserController::class);
+});
 
-    Route::middleware('can:admin')->group(function () {
-        Route::get('kelola-ruang', function () {
-            return view('admin.kelola');
-        })->name('kelola.admin');
+Route::middleware('can:admin')->group(function () {
+    Route::get('kelola-ruang', function () {
+        return view('admin.kelola');
+    })->name('kelola.admin');
 
-        Route::get('history-admin', function () {
-            return view('admin.history');
-        })->name('history.admin');
+    Route::get('history-admin', function () {
+        return view('admin.history');
+    })->name('history.admin');
 
-        Route::get('validasi-admin', function () {
-            return view('admin.validasi');
-        })->name('validasi.admin');
+    Route::get('validasi-admin', function () {
+        return view('admin.validasi');
+    })->name('validasi.admin');
 
-        Route::get('pelaporan-admin', function () {
-            return view('admin.pelaporan');
-        })->name('pelaporan.admin');
+    Route::get('pelaporan-admin', function () {
+        return view('admin.pelaporan');
+    })->name('pelaporan.admin');
 
-        Route::get('pinjam-admin', function () {
-            return view('admin.pinjam');
-        })->name('pinjam.admin');
+    Route::get('pinjam-admin', function () {
+        return view('admin.pinjam');
+    })->name('pinjam.admin');
 
-        Route::resource('sarpras', SarprasController::class);
-    });
+    Route::resource('sarpras', SarprasController::class);
+});
 
-    Route::middleware('can:user')->group(function () {
-        Route::get('daftar-sarana-prasarana-user', [UserSarprasController::class, 'index'])->name('daftarsaranaprasarana.user');
+Route::middleware('can:user')->group(function () {
+    Route::get('daftar-sarana-prasarana-user', [UserSarprasController::class, 'index'])->name('daftarsaranaprasarana.user');
 
 //        Route::get('daftar-sarana-prasarana-user', function () {
 //            return view('user.daftarsaranaprasarana');
 //        })->name('daftarsaranaprasarana.user');
 
-        Route::resource('peminjaman-user', PeminjamanUserController::class);
+    Route::resource('peminjaman-user', PeminjamanUserController::class);
 
-        Route::get('history-user', function () {
-            return view('user.history');
-        })->name('history.user');
+    Route::get('history-user', function () {
+        return view('user.history');
+    })->name('history.user');
 
-        Route::get('form-user', function () {
-            return view('user.form');
-        })->name('form.user');
-    });
+    Route::get('form-user', function () {
+        return view('user.form');
+    })->name('form.user');
 });
 
 //Route::get('/home', function () {
