683d2388347e86080c4bdf933b4a1df887369e1a
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-09 06:58:32 +0700

    Add : view kelola wewenang

---
+++ resources/views/superadmin/wewenang.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('title', 'Menu Kelola Wewenang')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Kelola Wewenang</h1>
+        </div>
+
+
+        <div class="section-body">
+            <div id="layoutSidenav_content">
+                <main>
+                    <!-- Main page content-->
+                    <div class="container-xl px-4 mt-n10">
+                        @if(session()->has('success'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('success') }}
+                            </div>
+
+                        @endif
+                        <div class="row">
+                            <div class="col-xl-6 mb-4">
+                                <!-- Dashboard example card 1-->
+                                <a class="card lift h-100" href="{{route('wewenang.create')}}">
+                                    <div class="card-body d-flex justify-content-center flex-column">
+                                        <div class="d-flex align-items-center justify-content-between">
+                                            <div class="me-3">
+                                                <i class="fas fa-user-plus" style="font-size: xx-large"></i>
+                                                <br><br>
+                                                <h5>Tambah Wewenang</h5>
+                                                <div class="text-muted small">Description</div>
+                                            </div>
+                                            <img src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/browser-stats.svg"
+                                                 alt="..." style="width: 8rem"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xl-6 mb-4">
+                                <!-- Dashboard example card 2-->
+                                <a class="card lift h-100" href="{{route('wewenang.index')}}">
+                                    <div class="card-body d-flex justify-content-center flex-column">
+                                        <div class="d-flex align-items-center justify-content-between">
+                                            <div class="me-3">
+                                                <i class="fas fa-user-edit" style="font-size: xx-large"></i>
+                                                <br><br>
+                                                <h5>Update Wewenang</h5>
+                                                <div class="text-muted small">Description</div>
+                                            </div>
+                                            <img src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/browser-stats.svg"
+                                                 alt="..." style="width: 8rem"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </section>
+@endsection
+
+
+@section('sidebar')
+    @parent
+    <li class="nav-item {{ request()->is('kelola-superadmin', 'users*') ? 'active' : '' }}"><a href="{{route('kelola.superadmin')}}" class="nav-link"><i class="fas fa-users"></i><span>Kelola Pengguna</span></a></li>
+    <li class="nav-item {{ request()->is('kelola-wewenang', 'users*') ? 'active' : '' }}"><a href="{{route('kelola.wewenang')}}" class="nav-link"><i class="fas fa-handshake"></i><span>Kelola Wewenang</span></a></li>
+@endsection
