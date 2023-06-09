ae353e2d7030ce5be7ce907ee829eb5c48b42bff
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-09 07:39:11 +0700

    Add : add feature add and delete wewenang

--- app/Http/Controllers/WewenangController.php
+++ app/Http/Controllers/WewenangController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\wewenang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class WewenangController extends Controller
 {
@@ -24,7 +26,7 @@ class WewenangController extends Controller
      */
     public function create()
     {
-        //
+        return view('superadmin.formtambahwewenang');
     }
 
     /**
@@ -32,7 +34,24 @@ class WewenangController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|max:100|min:3',
+            'pj' => 'required|max:200|min:3',
+            'jabatan' => 'required|max:50|min:3',
+            'nip' =>'required|numeric',
+            'ttd' => 'image|mimes:jpeg,png,jpg,svg|max:2048',
+        ]);
+
+        if ($request->file('ttd')) {
+            $file = $request->file('ttd');
+            $filename = 'ttdpj/' . time() . '_' . $file->getClientOriginalName();
+            $file->storeAs('', $filename);
+            $validatedData['ttd'] = $filename;
+        }
+
+        Wewenang::create($validatedData);
+
+        return redirect('kelola-wewenang')->with('success', 'Wewenang berhasil ditambahkan!');
     }
 
     /**
@@ -62,8 +81,17 @@ class WewenangController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(wewenang $wewenang)
+    public function destroy($id)
     {
-        //
+        $wewenang = Wewenang::findOrFail($id);
+
+        // Hapus file terkait jika ada
+        if ($wewenang->ttd) {
+            Storage::delete($wewenang->ttd);
+        }
+
+        $wewenang->delete();
+
+        return redirect()->back()->with('success', 'Wewenang berhasil dihapus!');
     }
 }
